816b0502a5af1f43c85bd0125c58a322945c410c
Elliot Waddington <elliot.waddington@autotrader.co.uk>
2020-11-11 19:21:56 +0000

    icon update

--- src/components/SortButton/SortButton.utils.tsx
+++ src/components/SortButton/SortButton.utils.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { FaSortAlphaDown, FaSortAlphaUpAlt, FaSortAmountUpAlt, FaSortAmountDown } from 'react-icons/fa';
+import { FaSortAlphaDown, FaSortAlphaDownAlt, FaSortAmountDownAlt, FaSortAmountDown } from 'react-icons/fa';
 import { Direction, SortResultsByOptions } from '../../types';
 
 export const getIcon = (type: SortResultsByOptions, direction: Direction): JSX.Element | null => {
   switch (type) {
     case SortResultsByOptions.ALPHABETICALLY:
-      return direction === Direction.ASC ? <FaSortAlphaDown /> : <FaSortAlphaUpAlt />;
+      return direction === Direction.ASC ? <FaSortAlphaDown size={20} /> : <FaSortAlphaDownAlt size={20} />;
     case SortResultsByOptions.STAR_RATING:
     case SortResultsByOptions.PRICE:
-      return direction === Direction.ASC ? <FaSortAmountUpAlt /> : <FaSortAmountDown />;
+      return direction === Direction.ASC ? <FaSortAmountDownAlt size={20} /> : <FaSortAmountDown size={20} />;
     default:
       return null;
   }
